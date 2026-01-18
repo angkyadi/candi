2f888948532836be4035011d04b1f470e884969b
angkyadi <angkyadiputra_2428250034@mhs.mdp.ac.id>
2026-01-18 22:19:34 +0700

    a

--- README.md
+++
@@ -1,3 +0,0 @@
-# wisatacandi
-
-A new Flutter project.
--- android/app/src/main/kotlin/com/example/wisatacandi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.wisatacandi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
